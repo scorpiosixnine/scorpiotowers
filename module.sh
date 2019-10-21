3310f6679b39d3343437bf91581da14be062cf2d
root <root@DESKTOP-D3974OQ.localdomain>
2019-10-21 21:07:32 +0100

    initial commit

---
+++ module.sh
@@ -0,0 +1,10 @@
+
+export MODULE_NAME=ScorpioTowers
+export MAJOR=1
+export MINOR=0
+export PATCH=0
+export GIT_NO=`git log --oneline | wc -l`
+export BUILD_NO=$(($GIT_NO + 20))
+
+echo "Module: $MODULE_NAME"
+echo "Version is $MAJOR.$MINOR.$PATCH ($BUILD_NO)"
---
+++ pull.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source "module.sh"
+scorpio-utilities/pull.sh
